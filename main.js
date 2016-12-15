c8fcbf6639788b635f10b23b8da91628c8f287fa
Travis Gorman <thefarbetween@me.com>
2016-12-15 16:01:39 -0600

    findMatches returns an array of matching objects

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
 // Get array of data from the endpoint
+// findMatches takes input patern and creates a regular expression to search for
+// `displayMatches` calls `findMatches`, maps over its returned data returning a string of list items, and inserts them into the `.suggestions` list.
+// `displayMatches` is an event handler, so `this.value` is anything currently typed in the search input
+
 
 const cities = [];
 
@@ -9,7 +13,6 @@ fetch(endpoint)
 		.then(data => cities.push(...data));
 
 
-// findMatches takes input patern and creates a regular expression to search for
 
 function findMatches(wordToMatch, cities){
 	return cities.filter(place => {
@@ -18,17 +21,11 @@ function findMatches(wordToMatch, cities){
 	});
 }
 
-// `displayMatches` calls `findMatches`, maps over its returned data returning a string of list items, and inserts them into the `.suggestions` list.
 
 function displayMatches() {
-	// this is an event handler, so `this.value` is anything currently typed in the search input
-	// call `findMatches` and pass in `this.value`, as the `wordsToMatch` argument, and the `cities` array
 	const matchArray = findMatches(this.value, cities);
 	// return an HTML string of list items by mapping over `matchArray`
 	const html = matchArray.map(place => {
-		// const RegEx = new RegExp(this.value, 'gi');
-		// const cityName = place.city.replace(regex,`<span class='hl'>${this.value}</span>`);
-		// const stateName = place.state.replace(regex,`<span class='hl'>${this.value}</span>`);
 		return `
 			<li>
 				<span class='name'>${place.city}, ${place.state}</span>
@@ -37,7 +34,6 @@ function displayMatches() {
 		`;
 	}).join('');
 	suggestions.innerHTML = html
-
 }
 
 // declare variables for search and display elements
@@ -57,3 +53,7 @@ searchInput.addEventListener('keyup', displayMatches);
 
 
 // highlight matching patterns in the displayed suggestions
+
+// const RegEx = new RegExp(this.value, 'gi');
+// const cityName = place.city.replace(regex,`<span class='hl'>${this.value}</span>`);
+// const stateName = place.state.replace(regex,`<span class='hl'>${this.value}</span>`);
